3990728266c796749f7d38dde007a4ef849df12e
DaeJungYoon <dbseowjd12@gmail.com>
2025-01-24 08:36:26 +0900

    20250122~ 20250124

---
+++ 20250122/bkj10926.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.read
+
+T = input().strip()
+message = T+"?!!"
+print(message)
---
+++ 20250124/bkj18108.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+changeYear = (T -543)
+
+print(changeYear)
