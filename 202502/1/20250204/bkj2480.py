507e2cda79e146d005c64e6afb2db867c4fce7b1
DaeJungYoon <dbseowjd12@gmail.com>
2025-02-04 12:55:33 +0900

    20250204 2

---
+++ 202502/1/20250204/bkj2480.py
@@ -0,0 +1,37 @@
+a, b, c =map(int,input().split())
+
+# if 같은 눈이 3개 나오면:
+#   print(10000 + a * 1000)
+# elif 같은 눈이 2개 나오면:
+#   print(1000 + 같은 눈 * 100)
+# elif a>b and a>c:
+#   print(a * 100)
+# elif b>c and b>a:
+#   print(b * 100)
+# elif c>a and c>b:
+#   print(c * 100)
+
+if a == b and b==c:
+  print(10000 + a * 1000)
+elif b==a or a==c:
+  print(1000 + a * 100)
+elif a==b or b==c:
+  print(1000 + b * 100)
+elif a==c or c==b:
+  print(1000 + c * 100)
+elif a>b and a>c:
+  print(a * 100)
+elif b>c and b>a:
+  print(b * 100)
+elif c>a and c>b:
+  print(c * 100)
+
+# 다른 사람 코드
+# if a==b==c:
+#   print(10000+a*1000)
+# elif a==b or b==c:
+#   print(1000+b*100)
+# elif a==c:
+#   print(1000+a*100)
+# else:
+#   print(max(a,b,c)*100)  
