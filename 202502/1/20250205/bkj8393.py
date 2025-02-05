f7860e172bc3962f43ab424300a03741f1cef783
DaeJungYoon <dbseowjd12@gmail.com>
2025-02-05 17:27:20 +0900

    20250205 bkj8393

---
+++ 202502/1/20250205/bkj8393.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+first = 0
+nums =[]
+for __ in range(n):
+  first +=1
+  # print(first)
+  nums.append(first)
+
+# print(nums)
+print(sum(nums))
