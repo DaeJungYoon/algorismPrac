ab22f310517d8120106f4b4fb02507ed0432cea2
DaeJungYoon <dbseowjd12@gmail.com>
2025-02-18 13:10:19 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,12 @@
+n,m = map(int, input().split())
+
+result= []
+
+for i in range(n):
+  result.append(i+1)
+
+for _ in range(m):
+  i, j = map(int,input().split())
+  result[i-1],result[j-1] = result[j-1],result[i-1]
+
+print(*result)
