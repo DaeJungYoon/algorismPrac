7a12ac85fd381d03eaf439a5eb2e6929f5ca3406
DaeJungYoon <dbseowjd12@gmail.com>
2025-03-10 20:13:21 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+count = 0
+
+for _ in range(n):
+  word = list(input())
+  newArr= []
+  newArr.append(word[0]) 
+
+  for i in range(len(word)-1):
+    if newArr[-1] != word[i+1]:
+      newArr.append(word[i+1])
+    
+  if len(newArr) == len(set(newArr)):
+    count +=1
+    
+print(count)
